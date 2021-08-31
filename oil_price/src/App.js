755e6a3e0836d47a692c036c7facf86bf990b9ef
ethanlee <seungwoonlee90@gmail.com>
2021-08-31 13:53:36 +0900

    create progress bar

--- oil_price/src/App.js
+++ oil_price/src/App.js
@@ -2,9 +2,27 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  let scrollTop = 0;
+  let bar;
+
+  window.onload = function() {
+      bar = document.getElementsByClassName("percentage")[0];
+  }
+
+  window.addEventListener("scroll" , function(event) {
+
+      scrollTop = document.documentElement.scrollTop;
+      let percent = Math.ceil((scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100);
+
+      bar.style.height = percent + "%"
+  });
+
   return (
     <div className="App">
       <h1>hi</h1>
+      <div className="progressbar">
+        <span className="percentage"></span>
+      </div>
     </div>
   );
 }

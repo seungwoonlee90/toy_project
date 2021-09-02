675b96233e81b5731773251bf0f92ea14eccac38
ethanlee <seungwoonlee90@gmail.com>
2021-09-02 13:06:22 +0900

    Update project

--- gasoline_project/main.js
+++ gasoline_project/main.js
@@ -47,3 +47,25 @@ window.onload = function() {
         behavior: 'smooth',
     })
 }
+
+let start = document.querySelector('button');
+
+start.addEventListener('click', function(event){
+    event.preventDefault();
+    const apiKey = document.querySelector('input').value;
+    api_call(apiKey);
+})
+
+function api_call(apiKey) {
+    let url = `http://www.opinet.co.kr/api/lowTop10.do?out=xml&code=${apiKey}&prodcd=B027&area=1703&cnt=10`
+
+    axios.request({
+        method: 'GET',
+        url: url,
+        headers: {'Content-Type': 'text/plain'},
+        type: 'jsonp'
+    })
+    .then(function(res) {
+        console.log(res.data)
+    })
+}

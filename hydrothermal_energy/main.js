0c2ec3afea51231435058229cf2a054f49068785
ethanlee <seungwoonlee90@gmail.com>
2021-10-06 10:01:04 +0900

    Split : hydrothermal_energy

--- hydrothermal_energy/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    axios.get('https://raw.githubusercontent.com/seungwoonlee90/toy_project/master/hydrothermal_energy/hydrothermal.json')
-    .then(function(res){
-        console.log(res.data)
-    })
-}

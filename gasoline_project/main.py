69e260732247e144ce312fa5dd13245abae1b4fa
ethanlee <seungwoonlee90@gmail.com>
2021-10-07 23:12:47 +0900

    Update : oil_price_seoul

--- gasoline_project/main.py
+++ gasoline_project/main.py
@@ -2,24 +2,47 @@ import os
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 service_key = ''
-url = f'http://www.opinet.co.kr/api/lowTop10.do?out=xml&code={service_key}&prodcd=B027&area=0102'
-res = requests.get(url)
+today = datetime.today().strftime("%Y-%m-%d")
+
+area_code = f'http://www.opinet.co.kr/api/areaCode.do?out=xml&code={service_key}&area=01'
+res = requests.get(area_code)
 soup = BeautifulSoup(res.content, 'html.parser')
+
+area = []
+for i in range(25) :
+    area_cd = soup.find_all('area_cd')[i].get_text()
+    area_nm = soup.find_all('area_nm')[i].get_text()
+    area.append([area_cd, area_nm])
+
+area = pd.DataFrame(area, columns = ['area_cd', 'area_nm'])
+area_cds = area['area_cd'].to_list()
+area_nms = area['area_nm'].to_list()
 
 
 df = []
 
-for i in range(10) :
-    price = soup.find_all('price')[i].get_text()
-    os_nm = soup.find_all('os_nm')[i].get_text()
-    van_adr = soup.find_all('van_adr')[i].get_text()
-    new_adr = soup.find_all('new_adr')[i].get_text()
-    xcoor = soup.find_all('gis_x_coor')[i].get_text()
-    ycoor = soup.find_all('gis_y_coor')[i].get_text()
-    df.append([os_nm, van_adr, new_adr, xcoor, ycoor, price])
+for area_cd, area_nm in zip(area_cds, area_nms) :
+    url = f'http://www.opinet.co.kr/api/lowTop10.do?out=xml&code={service_key}&prodcd=B027&area={area_cd}'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
 
-df = pd.DataFrame(df, columns = ['os_nm', 'van_adr', 'new_adr', 'x', 'y', 'price'])
-with open('./oil_price.json', 'w', encoding='utf-8') as file:
+    for i in range(10) :
+        try :
+            price = soup.find_all('price')[i].get_text()
+            os_nm = soup.find_all('os_nm')[i].get_text()
+            van_adr = soup.find_all('van_adr')[i].get_text()
+            new_adr = soup.find_all('new_adr')[i].get_text()
+            xcoor = soup.find_all('gis_x_coor')[i].get_text()
+            ycoor = soup.find_all('gis_y_coor')[i].get_text()
+            df.append([today, area_cd, area_nm, os_nm, van_adr, new_adr, xcoor, ycoor, price])
+        
+        except :
+            pass
+
+df = pd.DataFrame(df, columns = ['date', 'area_cd', 'area_nm', 'os_nm', 'van_adr', 'new_adr', 'x', 'y', 'price'])
+
+with open('./oil_price_seoul.json', 'w', encoding='utf-8') as file:
     df.to_json(file, force_ascii=False, orient='records')

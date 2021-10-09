b800cbd91a5c2efae71aadca9e96ec0671026ac8
ethanlee <seungwoonlee90@gmail.com>
2021-10-09 18:14:41 +0900

    Update : code edit

--- ohao_/이미지_병합/ohao.py
+++ ohao_/이미지_병합/ohao.py
@@ -1,18 +1,26 @@
+import os
 import glob
 from PIL import Image
 
 files = glob.glob("ohao/*.jpeg")
+files.sort(key=os.path.getmtime)
 
-canvas_width = 896
-canvas_height = 17654
+
+canvas_width = 1280
+canvas_height = len(files) * 2000
 
 canvas = Image.new('RGB', (canvas_width, canvas_height), 'white')
 output_height = 0
 
-for i in files :
-    image = Image.open(i)
+for i, file in enumerate(files) :
+    image = Image.open(file)
     width, height = image.size
-    canvas.paste(image, (0, output_height))
-    output_height += (height+160)
+    
+    if i == 0 :
+        canvas.paste(image, (140, output_height))
+        output_height += (height+200)
+    else :
+        canvas.paste(image, (0, output_height))
+        output_height += (height+200)
 
 canvas.save('ohao.jpg')

f802e3ac7c22301310eba44d545460911e3ade2e
ethanlee <seungwoonlee90@gmail.com>
2022-04-05 14:57:39 +0900

    anime.js

--- wedding/main.js
+++ wedding/main.js
@@ -1,95 +1,34 @@
 let scrollTop = 0;
-let t1 = gsap.timeline({ paused:true, reversed:true});
-let t2 = gsap.timeline({ paused:true, reversed:true});
-let t3 = gsap.timeline({ paused:true, reversed:true});
+let t1 = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 750
+  });
 
-t1.play();
+t1
+.add({
+  targets: '.menu',
+  opacity: 1,
+  translateX: [-100, 0],
+}
+)
 
-window.addEventListener("scroll", (e)=>{
-    scrollTop = document.documentElement.scrollTop;
-    console.log(scrollTop)
-        if( scrollTop > window.outerHeight / 10 ) {
-            t2.play();
-        } else if (scrollTop < window.outerHeight / 10) {
-            t2.reverse();
-        }
-        if ( scrollTop > 1000) {
-            t3.play();
-        } else if ( scrollTop < 1200) {
-            t3.reverse();
-        }
-    
-})
+.add({
+  targets: '.couple',
+  opacity: 1,
+  translateY: [500, 0],
+  easing: 'linear',
+}, 3)
 
-t1.from(".couple",
-{
-    ease: "slowmo.ease(.7, .8)",
-    y: "200%",
-    duration: 1.5,
-});
+.add({
+  targets: '.container h1',
+  opacity: 1,
+  translateY: [200, 0],
+  easing: 'linear',
+}, 3)
 
-t1.to(".menu",
-{
-    ease: "elastic.out(1, .8)",
-    opacity: 1,
-    duration: 1.5,
-},0.2
-);
-
-t1.to(".container h1, .container p",
-{
-    ease: "elastic.out(1, .8)",
-    y: "-8%",
-    opacity: 1,
-    duration: 1.5,
-},0.2
-);
-
-t2.to(".couple",
-{
-    ease: "ease.out",
-    y: "-120%",
-    duration: 2,
-});
-
-t2.to(".menu",
-{
-    ease: "power1.inOut",
-    x: "-250%",
-    opacity: 0,
-    duration: 1.5,
-}, 0.2);
-
-t2.to(".container h1, .container p, .container span",
-{
-    ease: "power1.inOut",
-    y: "500%",
-    opacity: 0,
-    duration: 1,
-},0.2
-);
-
-t2.to(".wedding",
-{
-    ease: "ease.inOut",
-    opacity: 1,
-    duration: 3,
-},0.2
-);
-
-t3.to(".wedding",
-{
-    ease: "ease.inOut",
-    opacity: 0,
-    duration: 3,
-},0.2
-);
-
-t3.to(".container h6",
-{
-    ease: "power1.inOut",
-    y: "500%",
-    opacity: 0,
-    duration: 1,
-},0.2
-);
+.add({
+  targets: '.container p',
+  opacity: 1,
+  translateY: [200, 0],
+  easing: 'linear',
+}, 3)

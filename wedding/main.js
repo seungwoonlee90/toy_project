0f20d385b3aaf6f3c83383db59ce21aff57051d1
ethanlee <seungwoonlee90@gmail.com>
2022-04-05 15:12:15 +0900

    anime.js seek

--- wedding/main.js
+++ wedding/main.js
@@ -1,7 +1,7 @@
 let scrollTop = 0;
 let t1 = anime.timeline({
     easing: 'easeOutExpo',
-    duration: 750
+    duration: 750,
   });
 
 t1
@@ -32,3 +32,27 @@ t1
   translateY: [200, 0],
   easing: 'linear',
 }, 3)
+
+
+let t2 = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 750,
+  });
+
+t2.add({
+  targets: '.container h2',
+  opacity: 1,
+  translateY: [500, 0],
+  easing: 'linear',  
+})
+
+t2.add({
+    targets: '.container h6',
+    opacity: [0, 1],
+    translateY: [500, 0],
+    easing: 'linear',  
+  })
+
+window.onscroll = function(e) {
+    t2.seek(window.pageYOffset * 2);
+}

af0ce727824f9df911e1bae1c58e82d48f92321d
seungwoonlee90 <seungwoonlee90@gmail.com>
2022-06-22 10:59:06 +0900

    fixed error, menu

--- 식단_자동화/main.py
+++ 식단_자동화/main.py
@@ -10,10 +10,8 @@ request = requests.get(url, verify=False)
 soup = BeautifulSoup(request.content, "html.parser")
 
 menus = soup.select("td[class=din_lists]")
-
-for i in menus :
-    menu = i.text
-    print(menu)
+menu = menus[0].text
+print('today menu:', menu)
 
 token = os.environ.get("SECURE") #token test
 slack = Slacker(token)

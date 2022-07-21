8bc6dcf14ef29cff6a3997a1dd82f27ee913a971
ethanlee <seungwoonlee90@gmail.com>
2022-07-21 20:04:41 +0900

    Update Intro page

--- wedding/main.js
+++ wedding/main.js
@@ -1,58 +1,37 @@
-let scrollTop = 0;
-let h1;
-let coverImg;
-let cover;
-let navBar = document.querySelector(".nav-bar");
-let menu = document.querySelector(".menu");
-let close = document.querySelector(".close");
-let greeting = document.querySelector("#greeting");
-let underline = document.querySelector(".underline");
-let origin = document.querySelectorAll("section")[0];
-let section = document.querySelectorAll("section");
-let greeting_btn = document.querySelector(".greeting-btn");
-let info_btn = document.querySelector(".info-btn");
-let location_btn = document.querySelector(".location-btn");
-let photo_btn = document.querySelector(".photo-btn");
-
 window.onload = function(){
-  coverImg = document.getElementsByClassName("coverImg")[0];
-  cover = document.getElementsByClassName("cover")[0];
-  cover.style.opacity = .3;
+  var section = document.getElementsByTagName("section");
+  var pointBtn = document.querySelectorAll('.pointWrap li');
+
+  var pageNum = 0;
+  var totalNum = section.length;
+
+  for( var i = 0; i < pointBtn.length; i++ ){
+    (function(idx) {
+        pointBtn[idx].onclick = function() {
+ 
+            pageNum = idx;
+            pageChangeFunc();
+            window.scrollTo({
+              top: section[pageNum].offsetTop,
+              behavior: 'smooth',
+            })
+        }
+    })(i);
+    
 }
 
-window.addEventListener("scroll", function(e){
-  scrollTop = document.documentElement.scrollTop;
-  coverImg.style.transform = "scale("+ (1 + scrollTop/1000) +")";
-  cover.style.opacity = .3 + scrollTop / 1000;
-
-  underline.style.width = ((scrollTop/ ( origin.offsetHeight)) * 60) + '%';
-  // check percentage;
-  if (scrollTop > 50) {
-    greeting.style.animation = "greeting 1.2s ease-out forwards";
+  //페이지 변경
+  function pageChangeFunc(){
+    for(var i=0; i<totalNum; i++){
+      section[i].classList.remove("active");
+      pointBtn[i].classList.remove("active");
+    }
+    section[pageNum].classList.add("active");
+    pointBtn[pageNum].classList.add("active");
+    
   }
 
-});
+  //페이지 로드되면 바로 실행
+  pageChangeFunc();
 
-menu.addEventListener("click", ()=>{
-  navBar.style.width = "60vw";
-})
-
-close.addEventListener("click", ()=>{
-  navBar.style.width = "0";
-})
-
-greeting_btn.addEventListener("click", ()=>{
-  window.scrollTo({ top: origin.offsetHeight, behavior: "smooth" });  
-})
-
-info_btn.addEventListener("click", ()=>{
-  window.scrollTo({ top: origin.offsetHeight + section[1].offsetHeight, behavior: "smooth" });  
-})
-
-location_btn.addEventListener("click", ()=>{
-  window.scrollTo({ top: origin.offsetHeight + section[1].offsetHeight + section[2].offsetHeight, behavior: "smooth" });  
-})
-
-photo_btn.addEventListener("click", ()=>{
-  alert("coming soon!")
-})
+}
